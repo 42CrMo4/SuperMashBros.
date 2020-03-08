ec16d1e8ffb53217bb80c31f0f5c6bf5f6e405ba
42CrMo4 <44754810+42CrMo4@users.noreply.github.com>
2020-03-08 21:30:36 +0100

    Changed LEDs to PL9823 (aka WS2812)
    Changed LEDs to programmable LEDs PL9823 (aka WS2812) Type.

--- int/int.ino
+++ int/int.ino
@@ -1,16 +1,17 @@
 /*
-
 SuperMashBros. 2020
-
 Spielmechanik für Cocktail Roboter
-
 */
+
+#include <FastLED.h>
 
 // setting up Pin arrays for buttons and LEDs to access them efficiently.
 //It is important that LED1 and Button1 are in the same location within their respective array
 const int commonPin = 2;
 int buttonPins[] = {4, 5, 6, 7};
-int ledPins[] = {10, 11, 12, 13};
+CRGB colourArray[] = {CRGB::Black,CRGB::Red,CRGB::Green,CRGB::Blue};
+#define NUM_LEDS 4
+#define DATA_PIN 53
 
 //misc
 int p = 0;
@@ -25,28 +26,32 @@ int max_interval_off = 700;
 int interval = 1000;           // interval at which to blink (milliseconds) for the first time
 
 //"sizeof" gives the size in bytes, so to get the number of elements it is neccessary to divide by one element
-int arraySize = (sizeof (ledPins) / sizeof(ledPins[0]));
+//int arraySize = (sizeof (ledPins) / sizeof(ledPins[0]));
+int arraySize = NUM_LEDS;
 
 //State arrays have fixed size because Arduino does not support adaptive "stl vectors" without extra libraries.
 // Increase if you want to add more than 15 leds/buttons
 bool buttonStateArray [15];
-bool ledStateArray [15];
+int ledStateArray [15];
 int onIntervalArray [15];
 int offIntervalArray [15];
 int currentMillisArray [15];
 int previousMillisArray [15];
 
+CRGB leds[NUM_LEDS];
 
 void setup()
 {
   //initialise the led pins (according to the array) as output
-  for (p = 0; p < arraySize; ++p) {
-    pinMode(ledPins[p], OUTPUT);
-  }
+//  for (p = 0; p < arraySize; ++p) {
+//    pinMode(ledPins[p], OUTPUT);
+//  }
 
   // interrupt button seting
   configureCommon(); // Setup pins for interrupt
   attachInterrupt(digitalPinToInterrupt(commonPin), pressInterrupt, FALLING); //interrupt
+
+  FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
 
   // Open the serial port at 9600 Bit/s:
   Serial.begin(9600);
@@ -60,25 +65,26 @@ void loop() {
     unsigned long currentMillis = millis();
     currentMillisArray[p] = currentMillis;
     //Read the ledState & buttonState of the respective array slot
-    ledStateArray[p] = digitalRead(ledPins[p]);
+//    ledStateArray[p] = digitalRead(ledPins[p]);
     //    buttonStateArray[p] = digitalRead(buttonPins[p]);
 
-    if ((ledStateArray[p] == HIGH) && (currentMillisArray[p] - previousMillisArray[p] >= onIntervalArray[p]))  {
+    if ((ledStateArray[p] > 0 ) && (currentMillisArray[p] - previousMillisArray[p] >= onIntervalArray[p]))  {
       //Set the duration of the next off interval for the respective array slot
       offIntervalArray[p] = random(min_interval_off, max_interval_off);
       //Change the led state
-      ledStateArray[p] = LOW;  // Turn it off
+      ledStateArray[p] = 0;  // Turn it off
       previousMillisArray[p] = currentMillisArray[p];  // Remember the time
-      digitalWrite(ledPins[p], ledStateArray[p]);  // Update the actual LED
+      leds[p] = colourArray[0];  // Update the actual LED
     }
-    else if ((ledStateArray[p] == LOW) && (currentMillisArray[p] - previousMillisArray[p] >= onIntervalArray[p])) {
+    else if ((ledStateArray[p] == 0 ) && (currentMillisArray[p] - previousMillisArray[p] >= onIntervalArray[p])) {
       //Set the duration of the next off interval for the respective array slot
       onIntervalArray[p] = random(min_interval_on, max_interval_on);
       //Change the led state
-      ledStateArray[p] = HIGH;  // turn it on
+      ledStateArray[p] = 1;  // turn it on
       previousMillisArray[p] = currentMillisArray[p];   // Remember the time
-      digitalWrite(ledPins[p], ledStateArray[p]);
-    }    // Update the actual LED
+      leds[p] = colourArray[1];  // Update the actual LED
+    }    
+    FastLED.show();
   }
 }
 
@@ -103,23 +109,24 @@ void pressInterrupt() { // ISR
 
 // compare button press to LED and count
 void press(int button) {
-  if (ledStateArray[button] == HIGH) {
-    if (countertemp == 0) {   // kann man das hier nicht auch in das obere if statement dazu geben?
-      ledStateArray[button] = LOW;
-      digitalWrite(ledPins[button], ledStateArray[button]);
+  if (ledStateArray[button] > 0 ) {
+    if (countertemp == 0) {   
+      ledStateArray[button] = 0;
+      leds[button] = colourArray[0];
+//      digitalWrite(ledPins[button], ledStateArray[button]);
       counter = counter + 1;
       Serial.println (counter);
     }
-    countertemp == 1;
+    countertemp == 1;   // Set if statement to false to stop the if loop
   }
-  else if (ledStateArray[button] == LOW) {
+  else if (ledStateArray[button] == 0) {
     if (countertemp == 0) {
       if (counter > 0) {
         counter = counter - 1;
       }
       Serial.println (counter);
     }
-    countertemp == 1;
+    countertemp == 1;   // Set if statement to false to stop the if loop
   }
 }
 
